31cc3ffec2272a6a619110d0a7eb04fa02c37eca
jonniebigodes <jonniebigodes@hotmail.com>
2020-02-19 16:37:53 +0000

    fix one more furio from sopranos moment....

--- src/pages/paymentreciept.js
+++ src/pages/paymentreciept.js
@@ -5,20 +5,9 @@ import Layout from "../components/layout"
  * @param {Object} location is destructured from the internal gatsby navigation api
  */
 const PaymentReciept = ({ location }) => {
-    // destructures the state passed via the payment form component
+  // destructures the state passed via the payment form component
   const { state } = location
 
-  // checks for state values, if they don't exist it might mean someone tried to access the route directly
-  if (state === null) {
-    return (
-      <Layout>
-        <h2> No payment information is present.</h2>
-      </Layout>
-    )
-  }
- 
-  // destructures the payment object that was injected through the gatsby navigation api
-  const { paymentInfo } = state
   return (
     <Layout>
       <>
@@ -28,7 +17,11 @@ const PaymentReciept = ({ location }) => {
         <h2 style={{ textAlign: "center" }}>
           Here's your payment reciept information:
         </h2>
-        <pre>{JSON.stringify(paymentInfo, null, 2)}</pre>
+        {state !== null ? (
+          <h2> No payment information is present.</h2>
+        ) : (
+          <pre>{JSON.stringify(state.paymentInfo, null, 2)}</pre>
+        )}
       </>
     </Layout>
   )
